42834c58d4e3f72e50dd58bed3f78973fa04594a
DeveloperYamin <developer.yamin@gmail.com>
2022-03-13 00:00:20 +0600

    Building the ChatInput Component !Done

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,22 +1,48 @@
 import { InfoOutlined, StarBorderOutlined } from "@mui/icons-material";
+import { useCollection, useDocument } from "react-firebase-hooks/firestore";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
+import { selectRoomId } from "../features/rooms/roomSlice";
+import { db } from "../firebase";
+import ChatInput from "./ChatInput";
 
 function Chat() {
+  const roomId = useSelector(selectRoomId);
+
+  const [roomDetails] = useDocument(
+    roomId && db.collection("rooms").doc(roomId)
+  );
+
+  const [roomMessages] = useCollection(
+    roomId &&
+      db
+        .collection("rooms")
+        .doc(roomId)
+        .collection("messages")
+        .orderBy("timestamp", "asc")
+  );
+
   return (
     <ChatContainer>
-      <ChatHeader>
-        <ChatHeaderLeft>
-          <h4>
-            <strong>#Room-Name</strong>
-          </h4>
-          <StarBorderOutlined />
-        </ChatHeaderLeft>
-        <ChatHeaderRight>
-          <p>
-            <InfoOutlined /> Details
-          </p>
-        </ChatHeaderRight>
-      </ChatHeader>
+      <>
+        <ChatHeader>
+          <ChatHeaderLeft>
+            <h4>
+              <strong>#Room-Name</strong>
+            </h4>
+            <StarBorderOutlined />
+          </ChatHeaderLeft>
+          <ChatHeaderRight>
+            <p>
+              <InfoOutlined /> Details
+            </p>
+          </ChatHeaderRight>
+        </ChatHeader>
+
+        <ChatMessages></ChatMessages>
+
+        <ChatInput channelId={roomId} channelName={roomDetails?.data().name} />
+      </>
     </ChatContainer>
   );
 }
@@ -29,6 +55,8 @@ const ChatContainer = styled.section`
   overflow: scroll;
   margin-top: 60px;
 `;
+
+const ChatMessages = styled.main``;
 
 const ChatHeader = styled.header`
   display: flex;
---
+++ src/components/ChatInput.jsx
@@ -0,0 +1,67 @@
+import { Button } from "@mui/material";
+import { serverTimestamp } from "firebase/firestore";
+import { useState } from "react";
+import styled from "styled-components";
+import { db } from "../firebase";
+
+function ChatInput({ channelName, channelId }) {
+  const [input, setInput] = useState("");
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+
+    if (!channelId) return false;
+
+    db.collection("rooms").doc(channelId).collection("messages").add({
+      message: input,
+      timestamp: serverTimestamp(),
+      user: "User Name",
+      userImage:
+        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    });
+
+    setInput("");
+  };
+
+  return (
+    <ChatInputContainer>
+      <form>
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+          placeholder={`Message ${channelName}`}
+        />
+        <Button type="submit" hidden onClick={sendMessage}>
+          Send
+        </Button>
+      </form>
+    </ChatInputContainer>
+  );
+}
+
+export default ChatInput;
+
+const ChatInputContainer = styled.footer`
+  border-radius: 20px;
+
+  > form {
+    position: relative;
+    display: flex;
+    justify-content: center;
+  }
+
+  > form > input {
+    position: fixed;
+    width: 60%;
+    bottom: 30px;
+    border: 1px solid gray;
+    border-radius: 3px;
+    padding: 20px;
+    outline: none;
+  }
+
+  > form > button {
+    display: none;
+  }
+`;
